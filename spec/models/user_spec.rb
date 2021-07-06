d8b338921c299ef9c96cab6572c0d55fe718e0ec
Henok <henatan99@gmail.com>
2021-07-06 15:33:40 +0300

    Add spec tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Association test
+  # ensure User model has a 1:m relationship with Measured model
+  it { should have_many(:measureds) }
+  # Validation test
+  # ensure column username is present before save
+  it { should validate_presence_of(:username) }
+  # ensure User model has a 1:, relationship with Measurement model through Measured
+  it { should have_many(:measurements).through(:measureds) }
 end
