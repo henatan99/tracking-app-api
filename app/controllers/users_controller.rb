2fa35213f5d19aa0990f5245a8eb10dd132d70cf
Henok <henatan99@gmail.com>
2021-07-06 20:09:42 +0300

    Add Response module with render_json method in controllers/concerns

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+    before_action :find_user, except: %i[create index]
+
+    # GET /users
+    def index
+        @users = User.all
+        json_response(@users)
+    end
+
+    # POST /users
+    def create
+        @user = User.create!(user_params)
+        json_response(@user, :created)
+    end
+
+    # GET /users/:id
+    def show
+        json_response(@user)
+    end
+
+    # PUT /users/:id
+    def update
+        @user.update(user_params)
+        head :no_content
+    end
+
+    # DELETE /users/:id
+    def destroy
+        @user.destroy
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        # whielist params
+        params.permit(:username)
+    end
+
+    def find_user
+        user = User.find(params[:id])
+    end
 end
