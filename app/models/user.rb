6a9bb7994f8cafbe71f00c2c9aa45866fa50888d
Henok <henatan99@gmail.com>
2021-07-11 16:14:13 +0300

    Add active record query for users table measureds column

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :measureds
+  has_many :measureds, -> { order(created_by: :desc) }
   has_many :measurements, through: :measureds
 
   validates_presence_of :username
