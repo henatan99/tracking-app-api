e519a64fa857f2ee323778fe2dde071a7f9f4843
Henok <henatan99@gmail.com>
2021-07-13 05:46:41 +0300

    Add goals to routes inide users routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :measureds, -> { order(created_by: :desc) }
   has_many :measurements, through: :measureds
+  has_many :goals
 
   validates :username, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users do
     resources :measureds
+    resources :goals
   end
   resources :measurements
 end
