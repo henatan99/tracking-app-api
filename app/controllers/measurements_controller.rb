5935b0cfc483d213ad71f194394933c50ecfe882
Henok <henatan99@gmail.com>
2021-07-20 04:30:03 +0300

    Add skip_before_action method for measurements index

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,4 +1,5 @@
 class MeasurementsController < ApplicationController
+  skip_before_action :require_login, only: [:index]
   before_action :find_measurement, except: %i[create index]
 
   # GET /measurements
