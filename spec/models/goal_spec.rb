bc9de0567dbdced6f36d1dcfd4266c32ee63ccfe
Henok <henatan99@gmail.com>
2021-08-08 00:22:24 +0300

    Add teste to goal model

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Goal, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:measurement) }
+
+  it { should validate_presence_of(:quantity) }
+  it { should validate_presence_of(:day_one) }
+  it { should validate_presence_of(:day_last) }
 end
