370660903de85a757d3576237b76e602c1107f30
Henok <henatan99@gmail.com>
2021-08-08 18:08:31 +0300

    Add model tests for measurement model

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Measurement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:measureds) }
+  it { should have_many(:goals) }
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:unit) }
 end
